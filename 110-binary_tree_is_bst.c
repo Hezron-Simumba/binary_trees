876b2cce38158c0d02053b2545fbf1b361ecb84f
Hezron-Simumba <simumbahezron@gmail.com>
2024-04-13 02:27:00 +0200

    Level-order traversal fix

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,37 +1,40 @@
 #include "binary_trees.h"
 
+int binary_tree_is_bst(const binary_tree_t *tree);
+int is_bst_helper(const binary_tree_t *tree, int low, int high);
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
+ * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree.
  *
- * Return: 1 if tree is a valid BST
- *         0 otherwise
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise.
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	return (btib_helper(tree, INT_MIN, INT_MAX));
+
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
 
 /**
- * btib_helper - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
- * @min: Lower bound of checked nored
- * @max: Upper bound of checked nodes
+ * is_bst_helper - Checks if a binary tree is a valid binary search tree.
  *
- * Return: 1 if tree is a valid BST
- *         0 otherwise
+ * @tree: A pointer to the root node of the tree to check.
+ * @low: The value of the smallest node visited thus far.
+ * @high: The value of the largest node visited this far.
+ *
+ * Return: If the tree is a valid BST, 1, otherwise, 0.
  */
-int btib_helper(const binary_tree_t *tree, int min, int max)
+int is_bst_helper(const binary_tree_t *tree, int low, int high)
 {
-	if (!tree)
-		return (1);
+	if (tree != NULL)
+	{
+		if (tree->n < low || tree->n > high)
+			return (0);
+		return (is_bst_helper(tree->left, low, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, high));
+	}
 
-	if (tree->n < min || tree->n > max)
-		return (0);
-
-	return (btib_helper(tree->left, min, tree->n - 1) &&
-		btib_helper(tree->right, tree->n + 1, max));
-	/* -1 and +1 stem from "There must be no duplicate values" req */
+	return (1);
 }
